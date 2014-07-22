f84d2de834fde24557b44b998d45204dccff41bf
Ryosuke　Hayashi <ryou.9514@gmail.com>
2014-07-22 23:19:32 +0900

    基本クラス追加

---
+++ core/setting.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+"""
+
+"""
+
+__author__ = 'Ryosuke'
+__date__ = '2014/07/19'
+
+
+class Setting(object):
+    def __init__(self):
+        pass
---
+++ core/timeline.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+"""
+
+"""
+
+__author__ = 'Ryosuke'
+__date__ = '2014/07/20'
+
+
+class TimeLine(object):
+    def __init__(self):
+        pass
