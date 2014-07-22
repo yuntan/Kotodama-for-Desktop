a7a3194ebd8c7ce4567cc41e71f7de7df4e5e6d7
Ryosuke　Hayashi <ryou.9514@gmail.com>
2014-07-22 23:16:53 +0900

    エントリポイント簡易実装

---
+++ kotodama.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+"""
+
+"""
+
+__author__ = 'Ryosuke'
+__date__ = '2014/07/18'
+
+import wx
+from core.account import Account
+from core.setting import Setting
+from core.timeline import TimeLine
+
+settings = Setting()
+accounts = Account()
+timeline = TimeLine()
+
+app = wx.App()
