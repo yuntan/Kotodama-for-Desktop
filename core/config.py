493f85b996a70491217027d699bdc05b603c0f2f
Ryosuke　Hayashi <ryou.9514@gmail.com>
2014-07-22 23:17:08 +0900

    設定値関連追加

---
+++ core/config.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+"""
+
+"""
+
+__author__ = 'Ryosuke'
+__date__ = '2014/07/20'
+
+import os
+
+
+APPLICATION_NAME = "言霊 for Desktop"
+APPLICATION_PATH = os.path.dirname(os.path.dirname(__file__))
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
